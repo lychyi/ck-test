54ea6f6af47fac63033aff8b683dc964154eafde
Lynn Chyi <lychyi@gmail.com>
2019-06-27 13:02:49 -0700

    Updates

---
+++ precache-manifest.ccbdf744197c00ee26e279247da4eed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80f6aa8e3b5bb1de392fabc4ca26783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e69af65cdc25a9ba1e",
+    "url": "/static/css/main.091f0dd8.chunk.css"
+  },
+  {
+    "revision": "b4c004c8f032f2286e14",
+    "url": "/static/js/2.2ba937ad.chunk.js"
+  },
+  {
+    "revision": "08e69af65cdc25a9ba1e",
+    "url": "/static/js/main.dc215936.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
